bfa9b4727577a290ed443200620f667af39176de
Sam Foster <sfoster@sneetchworks.com>
2013-10-09 13:52:26 -0700

    Basic migration working (for posts only). Automatic mapping of html to markdown not ideal, might need to also create an HTML post renderer. Also, need archive pages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var extend = hexo.extend,
   util = hexo.util,
   file = util.file,
+  path = require('path'),
   sourceDir = hexo.source_dir,
   xml2js = require('xml2js'),
   parser = new xml2js.Parser(),
@@ -9,9 +10,9 @@ var extend = hexo.extend,
   tomd = require('to-markdown').toMarkdown;
   isObject = function(thing) {
     return (thing && 'object' == typeof thing);
-  }
+  };
 
-extend.migrator.register('wordpress', function(args){
+extend.migrator.register('blogger', function(args){
   var source = args._.shift();
 
   if (!source) return console.log('\nUsage: hexo migrate blogger <source>');
@@ -36,21 +37,70 @@ extend.migrator.register('wordpress', function(args){
     },
     function(data, next){
       console.log('Analyzing.');
+      // just get the posts, ditch all the template, settings junk
+      // also excluding comments for now
+      var entries = data.feed.entry.filter(function(entry){
+        if(!entry.content) {
+          return false;
+        }
+        var term = (
+          entry.category && entry.category[0] &&
+          entry.category[0]['$'] && entry.category[0]['$'].term
+        );
+        if(term) {
+          return term.match(/kind#post$/);
+        }
+        return true;
+      });
 
-      file.write(sourceDir + 'posts.json', JSON.stringify(data, null, 2), next);
+      var dataFilename = (source.replace(/[\\\/]$/).split(/[\\\/]/)).pop();
+      dataFilename = dataFilename.replace(/\.[a-z]+/, '');
+      file.write(sourceDir + dataFilename + '.json', JSON.stringify(entries, null, 2), next);
 
-      var length = 0,
-        arr = []; // data.rss.channel[0].item;
+      var length = 0;
 
       // each post
-      async.forEach(arr, function(item, next){
-        // postTitle
-        // postDate
-        // postLink
-        // postContent
-        // postComment
-        // postStatus
-        // postTags or categories
+      function entryValue(entry, which) {
+        var value = entry[which][0];
+        if('object' == typeof value)
+          return value['_'];
+        else
+          return value;
+      }
+
+      async.forEach(entries, function(item, next){
+        var id = entryValue(item, 'id');
+        var _postLink  =  (item.link.filter(function(lnk){
+          return lnk['$'].type == "text/html" &&
+                 lnk['$'].rel == "alternate";
+        }))[0];
+        var postLink = _postLink['$'].href
+                            .substring(_postLink['$'].href.lastIndexOf('/')+1)
+                            .replace(/\.html$/, '');
+        console.log("postLink: "+postLink);
+        var postType = item.content[0] && item.content[0]['$'] && item.content[0]['$'].type;
+        var postTitle = entryValue(item, 'title');
+        var postDate =  entryValue(item, 'published');
+        var postContent = entryValue(item, 'content');
+        if('html' == postType) {
+          postContent = postContent.split('\n').map(function(line){
+            return line.trim();
+          }).join('\n');
+        }
+        if(!postTitle || (/^\s+$/).test(postTitle)) {
+          console.log("No postTitle, trying from content, postType is:" + postType);
+          postTitle = (postType == 'html') ? postContent.replace(/<\/?[^>]+>/g, '') :
+                                           postContent;
+          postTitle = postTitle.trim()
+                               .split(/\n|\\n/)[0]
+                               .trim()
+                               .replace(/[\-]{2,}/g, '--');
+        }
+        console.log("postTitle: " + postTitle);
+
+        var postComment = null;
+        var postStatus = '_posts';
+        var postTag, categories = null;
 
         var type = 'post'; // post||page,
         switch (type){
@@ -64,8 +114,10 @@ extend.migrator.register('wordpress', function(args){
               'categories: ' + (categories || 'uncategory'),
               '---'
             ];
-
-            file.write(sourceDir + postStatus + decodeURIComponent(postLink) + '.md', content.join('\n') + '\n\n' + postContent, next);
+            var filename = sourceDir + path.join(postStatus, decodeURIComponent(postLink) + '.md');
+            console.log("Creating post at: "+filename);
+            length++;
+            file.write(filename, content.join('\n') + '\n\n' + postContent, next);
             break;
 
           case 'page':
